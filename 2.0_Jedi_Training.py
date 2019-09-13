3a06758e6b48ae108f4a433c3ecabef98400ed91
cobbzac <cobbzac@urbandaleschools.com>
2019-09-13 13:59:16 -0500

    ch. 2 algebra

--- 2.0_Jedi_Training.py
+++ 2.0_Jedi_Training.py
@@ -1,43 +1,43 @@
 '''
-Sign your name:________________
+Sign your name: Zach Cobb
 
 1.) How do you enter a single line comment in a program?
+use a # and then type your comment behind it
 
 
 
-
-2.) Enter a=2 and b=5 in the Python Console window and then all of the following. 
+2.) Enter a=2 and b=5 in the Python Console window and then all of the following.
 What are the outputs?
 
-b/a                 =
-b//a                =
-b**a                =
-b%a                 =
-a+B                 =
-type(42)            =
-type(42.0)          =
-type("C3PO")        =
-type(True)          =
+b/a                 = 2.5
+b//a                = 2
+b**a                = 25
+b%a                 = 1
+a+B                 = 7
+type(42)            = <class 'int'>
+type(42.0)          = <class 'float'>
+type("C3PO")        = <class 'str'>
+type(True)          = <class 'bool'>
 
 
 
 3.) What is the final output of (a) and type(a) if you enter the following 5 lines
 into the Python Console Window?
 
-a=2
-a*=10
-a/=2
-a+=12
-a-=7
+a=2   2
+a*=10     20
+a/=2      10.0
+a+=12     22.0
+a-=7      15.0
 
-a             =
-type(a)       =
+a             = 15.00
+type(a)       = integer
 
 
 
 4.) Fix the mistake in the following code.
 x,y = (4,5)
-a = 3(x + y)
+a = 3*(x + y)
 a
 
 
@@ -50,8 +50,7 @@ x,y,z =(3,4,5)
 ave = x+y+z/3
 ave
 
-
-
+8.666
 
 
 '''
